6a985590ee6bfda0495681091567f708e3a10f41
Laszlo E Kovacs <laszloekovacs@gmail.com>
2025-05-04 15:59:09 +0200

    add more feature exploration

--- app/routes/_home.events.$eventId.tsx
+++ app/routes/_home.events.$eventId.tsx
@@ -11,8 +11,39 @@ export default function EventDetailPage({ loaderData }: Route.ComponentProps) {
 
 	return (
 		<div>
-			<h1>Event Detail Page for event id {eventId}</h1>
-			<p>This is the event detail page.</p>
+			<h1>Event Title for event id {eventId}</h1>
+			<p>Organizer: Mike</p>
+
+			<div>
+				<p>2025.06.06</p>
+				<p>Location: New York</p>
+				<p>Max players: 60</p>
+				<p>Avalilable slots: 10</p>
+
+				<p>Deadline: Apr. 30</p>
+			</div>
+
+			<div>
+				<p>Prices</p>
+				<p>Early bird: $50</p>
+				<p>Regular: $60</p>
+				<p>Last minute: $70</p>
+			</div>
+
+			<div>
+				<button>Apply</button>
+			</div>
+
+			<div>
+				<p>Tags</p>
+				<p>Milsim, retro, etc</p>
+			</div>
+
+			<div>
+				<p>Rules</p>
+				<p>1. No cheating</p>
+				<p>2. No stealing</p>
+			</div>
 		</div>
 	)
 }
