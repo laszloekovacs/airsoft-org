f6a78a675d7a02335b336526c942fe7edaa25e63
laszloekovacs <laszloekovacs@gmail.com>
2025-05-05 08:49:21 +0200

    feat: event creation validation using Zod

---
+++ app/routes/dashboard.event.$eventId.tsx
@@ -0,0 +1,7 @@
+export default function DashboardEventsEventId() {
+	return (
+		<div>
+			<h1>Edit Event</h1>
+		</div>
+	)
+}
--- app/routes/dashboard.event.create.tsx
+++ app/routes/dashboard.event.create.tsx
@@ -1,38 +1,56 @@
-import { Form, Link, useActionData } from 'react-router'
+import { Form, Link, useActionData, useFetcher } from 'react-router'
+import { z } from 'zod'
 
-export default function DashboardEventCreate() {
-	const data = useActionData()
 
-	return (
-		<div>
-			<h1>uj esemeny letrehozasa</h1>
-
-			<Form method='post'>
-				<label htmlFor='name'>esemeny neve</label>
-				<input type='text' name='name' id='name' required />
-				<label htmlFor='date'>esemeny idopontja</label>
-				<input type='date' name='date' id='date' required />
-				<input type='submit' value='letrehozas' />
-			</Form>
-
-			{data && (
-				<div>
-					<p>{data.name} nevu esemeny letrehozva!</p>
-					<Link to={`/dashboard/event/${data.name}`}>esemeny szerkesztese</Link>
-				</div>
-			)}
-		</div>
-	)
-}
-
-// should return a generated event link, or an error message
 export async function action({ request }: { request: Request }) {
 	const formData = await request.formData()
 	const name = formData.get('name')
 	const date = formData.get('date')
 
-	// TODO: validate name and date
-	// return error if already exists
 
-	return { name, date } // TODO: return generated event link
+	const createEventSchema = z.object({
+		name: z.string().length(3, 'legalább 3 karakter hosszú név szükséges'),
+
+		date: z
+			.date({
+				required_error: 'az esemény időpontja kötelező',
+				invalid_type_error: 'az esemény időpontja nem megfelelő formátumú'
+			})
+			.min(new Date(), 'az esemény időpontja nem lehet a múltban')
+	})
+
+	const parseResult = createEventSchema.safeParse({ name, date })
+	return parseResult
+}
+
+
+export default function DashboardEventCreate() {
+	const fetcher = useFetcher<typeof action>()
+
+	return (
+		<div>
+			<h1>Új esemény létrehozása</h1>
+
+			<fetcher.Form method='post'>
+				<label htmlFor='name'>esemény neve</label>
+				<input type='text' name='name' id='name' required />
+
+				<label htmlFor='date'>esemény időpontja</label>
+				<input type='date' name='date' id='date' required />
+
+				<input type='submit' value='létrehozás' />
+			</fetcher.Form>
+
+			{fetcher.data && (
+				<div>
+					<h2>uj esemeny letrehozva</h2>
+					<p>esemeny neve: {fetcher.data.}</p>
+					<p>esemeny idopontja: {fetcher.data.date}</p>
+					<Link to={`/dashboard/event/${fetcher.data.name}`}>
+						létrehozott esemény szerkesztése
+					</Link>
+				</div>
+			)}
+		</div>
+	)
 }
