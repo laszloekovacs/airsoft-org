0f5ef25d442461d6dd5f280c86917a81f9448d05
laszloekovacs <laszloekovacs@gmail.com>
2025-05-06 13:08:25 +0200

    refactor: remove usersTable from schema and update drizzle config to include auth-schema

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,11 +1,5 @@
 import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
-export const usersTable = sqliteTable('users', {
-	id: int('id').primaryKey({ autoIncrement: true }),
-	name: text('name').notNull(),
-	email: text('email').notNull().unique()
-})
-
 export const eventsTable = sqliteTable('events', {
 	id: int('id').primaryKey({ autoIncrement: true }),
 	title: text('name').notNull(),
