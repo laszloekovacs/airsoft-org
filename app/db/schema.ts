386094d61e5b0b03eeb41a261804e049a38c1164
Laszlo E Kovacs <laszloekovacs@gmail.com>
2025-05-06 20:32:25 +0200

    add schema for applications

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,4 +1,5 @@
-import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core'
+import { int, sqliteTable, text, unique } from 'drizzle-orm/sqlite-core'
+import { user } from './auth-schema'
 
 export const eventsTable = sqliteTable('events', {
 	id: int('id').primaryKey({ autoIncrement: true }),
@@ -9,3 +10,20 @@ export const eventsTable = sqliteTable('events', {
 		.notNull()
 		.$default(() => new Date().toISOString())
 })
+
+export const applicationsTable = sqliteTable(
+	'applications',
+	{
+		id: int('id').primaryKey({ autoIncrement: true }),
+		eventId: int('event_id')
+			.notNull()
+			.references(() => eventsTable.id, { onDelete: 'set null' }),
+		userId: text('user_id')
+			.notNull()
+			.references(() => user.id, { onDelete: 'set null' }),
+		createdAt: text('created_at')
+			.notNull()
+			.$default(() => new Date().toISOString())
+	},
+	table => [unique().on(table.eventId, table.userId)]
+)
