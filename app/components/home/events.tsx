898368063edd6a17979731dc70c7915ae2d72580
laszloekovacs <laszloekovacs@gmail.com>
2025-05-06 11:47:27 +0200

    refactor: update EventList and EventItem components to use InferSelectModel and streamline event data handling

--- app/components/home/events.tsx
+++ app/components/home/events.tsx
@@ -1,38 +1,35 @@
+import type { InferSelectModel } from 'drizzle-orm'
 import { Link } from 'react-router'
+import { eventsTable } from '~/db/schema'
 
-type EventItemPropType = {
-	id: number
-	title: string
-	description: string
-	date: string
-	location: string
-}
-
-export const EventList = (props: { events: EventItemPropType[] }) => {
-	const { events } = props
+export const EventList = (props: {
+	eventList: Array<InferSelectModel<typeof eventsTable>>
+}) => {
+	const { eventList } = props
 
 	return (
 		<div>
 			<h1>Upcomming events</h1>
-			{events.map(e => (
-				<div key={e.id}>
-					<EventItem event={e} />
-				</div>
-			))}
+			<ul>
+				{eventList.map(e => (
+					<EventItem key={e.id} event={e} />
+				))}
+			</ul>
 		</div>
 	)
 }
 
-export const EventItem = (props: { event: EventItemPropType }) => {
-	const { title, description, date, location } = props.event
+export const EventItem = (props: {
+	event: InferSelectModel<typeof eventsTable>
+}) => {
+	const { title, date } = props.event
 	return (
-		<div>
-			<Link to={`/events/${props.event.id}`}>
+		<li>
+			<Link to={`/events/${props.event.urlSlug}`}>
 				<h2>{title}</h2>
 			</Link>
-			<p>{description}</p>
+
 			<p>{date}</p>
-			<p>{location}</p>
-		</div>
+		</li>
 	)
 }
--- app/routes/_home._index.tsx
+++ app/routes/_home._index.tsx
@@ -1,47 +1,21 @@
 import type { Route } from './+types/_home._index'
 import { EventList } from '../components/home/events'
+import db from '~/services/db.server'
+import { eventsTable } from '~/db/schema'
+import { eq } from 'drizzle-orm'
 
 export const loader = async ({ params }: Route.LoaderArgs) => {
-	const eventsData = [
-		{
-			id: 1,
-			title: 'Event 1',
-			description: 'Description for Event 1',
-			date: '2023-10-01',
-			location: 'Location 1'
-		},
-		{
-			id: 2,
-			title: 'Event 2',
-			description: 'Description for Event 2',
-			date: '2023-10-02',
-			location: 'Location 2'
-		},
-		{
-			id: 3,
-			title: 'Event 3',
-			description: 'Description for Event 3',
-			date: '2023-10-03',
-			location: 'Location 3'
-		},
-		{
-			id: 4,
-			title: 'Event 4',
-			description: 'Description for Event 4',
-			date: '2023-10-04',
-			location: 'Location 4'
-		}
-	]
+	const eventList = await db.select().from(eventsTable)
 
-	return { eventsData }
+	return { eventList }
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-	const { eventsData } = loaderData
+	const { eventList } = loaderData
 
 	return (
 		<div>
-			<EventList events={eventsData} />
+			<EventList eventList={eventList} />
 		</div>
 	)
 }
