b929847ef46f824c59078234bf5f4c3492e5f57d
laszloekovacs <laszloekovacs@gmail.com>
2025-05-07 11:33:12 +0200

    updated

--- app/services/db.server.ts
+++ app/services/db.server.ts
@@ -1,5 +1,9 @@
 import { drizzle } from 'drizzle-orm/node-postgres'
 
+if (typeof process.env.DATABASE_URL != "string") {
+    throw new Error('DATABASE_URL is not defined'))
+}
+
 const db = drizzle(process.env.DATABASE_URL!)
 
 export default db
