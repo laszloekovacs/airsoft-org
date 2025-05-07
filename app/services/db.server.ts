a2e4f7903940697a6a261dfa2035c302ed15a83e
laszloekovacs <laszloekovacs@gmail.com>
2025-05-07 11:37:04 +0200

    fix: removed typo

--- app/services/db.server.ts
+++ app/services/db.server.ts
@@ -1,7 +1,7 @@
 import { drizzle } from 'drizzle-orm/node-postgres'
 
-if (typeof process.env.DATABASE_URL != "string") {
-    throw new Error('DATABASE_URL is not defined'))
+if (typeof process.env.DATABASE_URL != 'string') {
+	throw new Error('DATABASE_URL is not defined')
 }
 
 const db = drizzle(process.env.DATABASE_URL!)
