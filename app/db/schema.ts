7e8b072053408a19e10281b9357158ffb9575886
laszloekovacs <laszloekovacs@gmail.com>
2025-05-10 20:03:06 +0200

    cleaned up package files

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,5 +1,4 @@
 import {
-	uuid,
 	integer,
 	text,
 	pgTable,
--- app/services/db.server.ts
+++ app/services/db.server.ts
@@ -1,6 +1,7 @@
 import { drizzle } from 'drizzle-orm/node-postgres'
 
 if (typeof process.env.DATABASE_URL != 'string') {
+	console.log(process.env.DATABASE_URL)
 	throw new Error('DATABASE_URL is not defined')
 }
 
