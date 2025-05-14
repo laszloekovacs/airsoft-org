95a4f87ef6fafc3c00759d467864d6d734d8a5db
laszloekovacs <laszloekovacs@gmail.com>
2025-05-14 10:19:39 +0200

    added data loader to dashboard

--- app/routes/dashboard/route.tsx
+++ app/routes/dashboard/route.tsx
@@ -1,7 +1,18 @@
 import { Link, Outlet } from 'react-router'
 import Sitemap from '~/routes/_home/sitemap'
+import type { Route } from './+types/route'
+import db from '~/services/db.server'
+import { eventsTable } from '~/db/schema'
 
-export default function DashboardIndex() {
+export const loader = async ({ params }: Route.LoaderArgs) => {
+	const events = await db.select().from(eventsTable)
+
+	return { events }
+}
+
+export default function DashboardIndex({ loaderData }: Route.ComponentProps) {
+	const { events } = loaderData
+
 	return (
 		<div>
 			<Link to='/dashboard'>
