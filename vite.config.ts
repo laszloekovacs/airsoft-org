de03222b1681b4b605e8fe4f5cf034bd7ebde4e3
laszloekovacs <laszloekovacs@gmail.com>
2025-05-16 18:07:12 +0200

    removed tailwindcss deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { reactRouter } from '@react-router/dev/vite'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
 import { reactRouterHonoServer } from 'react-router-hono-server/dev'
 
 export default defineConfig({
 	plugins: [
-		tailwindcss(),
 		reactRouter(),
 		tsconfigPaths(),
 		reactRouterHonoServer({ runtime: 'bun' })
