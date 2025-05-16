283e1e612d32db982f8139906a8c284771932e88
laszloekovacs <laszloekovacs@gmail.com>
2025-05-16 17:30:13 +0200

    fix: enable health check endpoint and update router config

--- app/server.ts
+++ app/server.ts
@@ -5,6 +5,10 @@ import { createHonoServer } from 'react-router-hono-server/bun'
 
 // if you want to store clients, tho some db would be more fitting
 //const clients = new Set<WSContext>()
-//const app = new Hono()
+const app = new Hono()
 
-export default await createHonoServer()
+app.get('/health', c => c.json({ status: 'ok' }))
+
+export default await createHonoServer({
+	app
+})
