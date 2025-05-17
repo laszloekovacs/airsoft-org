204b07c982f160e4db87a90fad4c996527e8a5ac
laszloekovacs <laszloekovacs@gmail.com>
2025-05-17 21:00:36 +0200

    refactor layout structure in DashboardIndex component

--- app/routes/dashboard/route.tsx
+++ app/routes/dashboard/route.tsx
@@ -5,11 +5,13 @@ import Sitemap from '~/components/sitemap'
 export default function DashboardIndex() {
 	return (
 		<PageLayout>
-			<Link to='/dashboard'>
-				<h1>Dashboard</h1>
-			</Link>
+			<div>
+				<Link to='/dashboard'>
+					<h1>Dashboard</h1>
+				</Link>
 
-			<Link to='/dashboard/event/create'>új esemény</Link>
+				<Link to='/dashboard/event/create'>új esemény</Link>
+			</div>
 
 			<Outlet />
 			<Sitemap />
