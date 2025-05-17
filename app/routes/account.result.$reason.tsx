da993b92304038d8644ca2c4e36b5bc833f0c0c0
laszloekovacs <laszloekovacs@gmail.com>
2025-05-17 19:44:15 +0200

    feat: implement signup success message and navigation link

--- app/routes/account.result.$reason.tsx
+++ app/routes/account.result.$reason.tsx
@@ -1,7 +1,22 @@
-import { useParams } from 'react-router'
+import { Link, useParams } from 'react-router'
 
 export default function AccountActionResult() {
 	const params = useParams<string>()
 
-	return <pre>{JSON.stringify(params)}</pre>
+	const signup = (
+		<div>
+			<h1>Sikeres regisztráció!</h1>
+			<Link to='/'>vissza a főoldalra</Link>
+		</div>
+	)
+
+	switch (params.reason) {
+		case 'signup':
+			return signup
+			break
+
+		default:
+			throw new Error('hibás oldal paraméter')
+			break
+	}
 }
