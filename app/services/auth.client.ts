e078c2135b17cee6ea35674b3dc4d1f14ac8630b
laszloekovacs <laszloekovacs@gmail.com>
2025-05-17 21:33:44 +0200

    add basename

--- app/services/auth.client.ts
+++ app/services/auth.client.ts
@@ -3,4 +3,5 @@ import { createAuthClient } from 'better-auth/react'
 // TODO: Update the baseURL
 export const authClient = createAuthClient({
 	//baseURL: process.env.BASE_URL!
+	baseURL: window.location.hostname
 })
