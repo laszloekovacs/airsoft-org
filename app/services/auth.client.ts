6d018bfbfdd3bf5faaab03c8b236bf21cb6ed156
laszloekovacs <laszloekovacs@gmail.com>
2025-05-18 16:52:50 +0200

    feat: update font styles to use 'Work Sans' and adjust typography variables

--- app/services/auth.client.ts
+++ app/services/auth.client.ts
@@ -2,5 +2,6 @@ import { createAuthClient } from 'better-auth/react'
 // change import from better-auth/react
 // TODO: Update the baseURL
 export const authClient = createAuthClient({
-	baseURL: window.location.protocol + window.location.hostname
+	//baseURL: window.location.protocol + window.location.hostname
+	baseURL: import.meta.env.VITE_BASE_URL
 })
