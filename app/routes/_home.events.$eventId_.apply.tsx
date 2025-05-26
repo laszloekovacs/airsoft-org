fb3c36c445e57f7ba5be81893cf1e129d680667e
Laszlo E Kovacs <laszloekovacs@gmail.com>
2025-05-26 10:42:28 +0200

    Shadcn integration (#4)
    * added ui
    
    * add styling
    
    * fix tw error
    
    * updated

--- app/routes/_home.events.$eventId_.apply.tsx
+++ app/routes/_home.events.$eventId_.apply.tsx
@@ -1,83 +1,88 @@
-import { useFetcher } from 'react-router'
-import type { Route } from './+types/_home.events.$eventId_.apply'
-import { AuthenticatedOnly, authServer } from '~/services/auth.server'
-import db from '~/services/db.server'
-import { userAtEventTable, eventRecord } from '~/schema/schema'
-import { eq } from 'drizzle-orm'
+import { useFetcher } from "react-router";
+import type { Route } from "./+types/_home.events.$eventId_.apply";
+import { AuthenticatedOnly, authServer } from "~/services/auth.server";
+import db from "~/services/db.server";
+import { userAtEventTable, eventRecord } from "~/schema/schema";
+import { eq } from "drizzle-orm";
+import { Label } from "~/components/ui/label";
+import { Checkbox } from "~/components/ui/checkbox";
+import { Button } from "~/components/ui/button";
 
 export const loader = async ({ request }: Route.LoaderArgs) => {
-	AuthenticatedOnly(request)
+	AuthenticatedOnly(request);
 
-	return {}
-}
+	return {};
+};
 
 export default function ApplyEventPage() {
-	const fetcher = useFetcher()
+	const fetcher = useFetcher();
 
 	return (
 		<div>
 			<h2>Jelentkezes jatekra</h2>
 			<p>feltetelek itt</p>
 
-			<fetcher.Form method='post'>
-				<label>
+			<fetcher.Form method="post">
+				<Label>
 					<span>elfogadom a felteteleket</span>
-					<input type='checkbox' name='acceptTerms' id='acceptTerms'></input>
-				</label>
+					<Checkbox name="acceptTerms" id="acceptTerms" />
+				</Label>
 				<br />
-				<input type='hidden' name='intent' value='apply' />
-				<input type='submit' value='Jelentkezek' />
+				<input type="hidden" name="intent" value="apply" />
+				<Button type="submit" value="Jelentkezek">
+					Jelentkezek
+				</Button>
 			</fetcher.Form>
 		</div>
-	)
+	);
 }
 
 type ErrorResponse = {
-	status: 'error'
-	reason: string
-}
+	status: "error";
+	reason: string;
+};
 
 type SuccessResponse = {
-	status: 'success'
-	message: string
-}
+	status: "success";
+	message: string;
+};
 
-type ActionResponse = ErrorResponse | SuccessResponse
+type ActionResponse = ErrorResponse | SuccessResponse;
 
 export async function action({
 	request,
-	params
+	params,
 }: Route.ActionArgs): Promise<ActionResponse> {
-	AuthenticatedOnly(request)
-	const sessionData = await authServer.api.getSession(request)
+	AuthenticatedOnly(request);
+	const sessionData = await authServer.api.getSession(request);
 
 	if (!sessionData) {
-		throw new Error('Session not found')
+		throw new Error("Session not found");
 	}
 
-	const { user } = sessionData
+	const { user } = sessionData;
 
 	// find event id from stub
 	const event = await db
 		.select()
 		.from(eventRecord)
-		.where(eq(eventRecord.slug, params.eventId))
+		.where(eq(eventRecord.slug, params.eventId));
 
 	// create an entry in the application
 	const result = await db.insert(userAtEventTable).values({
 		userId: user.id,
-		eventId: event[0].id
-	})
+		eventId: event[0].id,
+	});
 
 	if (result.rowCount != 1) {
 		return {
-			status: 'error',
-			reason: 'Sikertelen jelentkezés!'
-		}
+			status: "error",
+			reason: "Sikertelen jelentkezés!",
+		};
 	}
 
 	return {
-		status: 'success',
-		message: 'Sikeres jelentkezés!'
-	}
+		status: "success",
+		message: "Sikeres jelentkezés!",
+	};
 }
--- app/routes/dashboard.event.$eventId.register.tsx
+++ app/routes/dashboard.event.$eventId.register.tsx
@@ -1,36 +1,36 @@
-import { userAtEventTable, eventRecord } from '~/schema/schema'
-import db from '~/services/db.server'
-import type { Route } from './+types/dashboard.event.$eventId.register'
-import { eq } from 'drizzle-orm'
-import { user } from '~/schema/auth-schema'
-import { useActionData, useFetcher } from 'react-router'
-import { Button } from '~/components/Button'
+import { userAtEventTable, eventRecord } from "~/schema/schema";
+import db from "~/services/db.server";
+import type { Route } from "./+types/dashboard.event.$eventId.register";
+import { eq } from "drizzle-orm";
+import { user } from "~/schema/auth-schema";
+import { useActionData, useFetcher } from "react-router";
+import { Button } from "~/components/ui/button";
 
 export const loader = async ({ request, params }: Route.LoaderArgs) => {
 	// TODO get user and check if event is his
 	// check for claim that he's an organizer
-	const filter = { id: userAtEventTable.id }
+	const filter = { id: userAtEventTable.id };
 
 	const data = await db
 		.select()
 		.from(userAtEventTable)
 		.leftJoin(eventRecord, eq(userAtEventTable.eventId, eventRecord.id))
 		.leftJoin(user, eq(userAtEventTable.userId, user.id))
-		.where(eq(eventRecord.slug, params.eventId))
+		.where(eq(eventRecord.slug, params.eventId));
 
-	return data
-}
+	return data;
+};
 
 export default function RegistrationPage({ loaderData }: Route.ComponentProps) {
-	const fetcher = useFetcher()
-	const actionData = useActionData<{ status: 'ok' | 'error' }>()
+	const fetcher = useFetcher();
+	const actionData = useActionData<{ status: "ok" | "error" }>();
 
 	const processApplicationApproval = async () => {
 		await fetcher.submit(
-			{ intent: 'confirm', eventId: '1' },
-			{ method: 'post' }
-		)
-	}
+			{ intent: "confirm", eventId: "1" },
+			{ method: "post" },
+		);
+	};
 
 	return (
 		<div>
@@ -45,32 +45,32 @@ export default function RegistrationPage({ loaderData }: Route.ComponentProps) {
 					))}
 			</ul>
 
-			<div>{actionData && actionData.status}</div>
+			<div>{actionData?.status}</div>
 
 			<div>
 				<pre>{JSON.stringify(loaderData, null, 2)}</pre>
 			</div>
 		</div>
-	)
+	);
 }
 
 export const action = async ({ request }: Route.ActionArgs) => {
-	const actionData = await request.formData()
-	const intent = actionData.get('intent')?.toString()
-	const eventId = actionData.get('id')?.toString() ?? ''
+	const actionData = await request.formData();
+	const intent = actionData.get("intent")?.toString();
+	const eventId = actionData.get("id")?.toString() ?? "";
 
-	if (intent == 'confirm') {
-		console.log('confirmed')
+	if (intent == "confirm") {
+		console.log("confirmed");
 
 		// set isConfirmed to true
 
 		const result = await db
 			.update(userAtEventTable)
 			.set({ isConfirmed: true })
-			.where(eq(userAtEventTable.id, parseInt(eventId)))
+			.where(eq(userAtEventTable.id, Number.parseInt(eventId)));
 
-		return { status: 'ok' }
+		return { status: "ok" };
 	}
 
-	return { status: 'error' }
-}
+	return { status: "error" };
+};
