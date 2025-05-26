9211901777167b358e4444f86747962ff38e1f32
laszloekovacs <laszloekovacs@gmail.com>
2025-05-26 13:26:28 +0200

    refactor: clean up code formatting and remove unnecessary semicolons

--- app/routes/dashboard.event.$eventId.register.tsx
+++ app/routes/dashboard.event.$eventId.register.tsx
@@ -1,48 +1,47 @@
-import { userAtEventTable, eventRecord } from "~/schema/schema";
-import db from "~/services/db.server";
-import type { Route } from "./+types/dashboard.event.$eventId.register";
-import { eq } from "drizzle-orm";
-import { user } from "~/schema/auth-schema";
-import { useActionData, useFetcher } from "react-router";
-import { Button } from "~/components/ui/button";
+import { userAtEventTable, eventRecord } from "~/schema/schema"
+import db from "~/services/db.server"
+import type { Route } from "./+types/dashboard.event.$eventId.register"
+import { eq } from "drizzle-orm"
+import { user } from "~/schema/auth-schema"
+import { useActionData, useFetcher } from "react-router"
+import { Button } from "~/components/ui/button"
 
 export const loader = async ({ request, params }: Route.LoaderArgs) => {
 	// TODO get user and check if event is his
 	// check for claim that he's an organizer
-	const filter = { id: userAtEventTable.id };
+	const filter = { id: userAtEventTable.id }
 
 	const data = await db
 		.select()
 		.from(userAtEventTable)
 		.leftJoin(eventRecord, eq(userAtEventTable.eventId, eventRecord.id))
 		.leftJoin(user, eq(userAtEventTable.userId, user.id))
-		.where(eq(eventRecord.slug, params.eventId));
+		.where(eq(eventRecord.slug, params.eventId))
 
-	return data;
-};
+	return data
+}
 
 export default function RegistrationPage({ loaderData }: Route.ComponentProps) {
-	const fetcher = useFetcher();
-	const actionData = useActionData<{ status: "ok" | "error" }>();
+	const fetcher = useFetcher()
+	const actionData = useActionData<{ status: "ok" | "error" }>()
 
 	const processApplicationApproval = async () => {
 		await fetcher.submit(
 			{ intent: "confirm", eventId: "1" },
 			{ method: "post" },
-		);
-	};
+		)
+	}
 
 	return (
 		<div>
 			<ul>
-				{loaderData &&
-					loaderData.map((attendee) => (
-						<li key={attendee.user_at_event.id}>
-							<span>{attendee.user?.name}</span>
-							<br />
-							<Button onClick={processApplicationApproval}>megerosit</Button>
-						</li>
-					))}
+				{loaderData?.map((attendee) => (
+					<li key={attendee.user_at_event.id}>
+						<span>{attendee.user?.name}</span>
+						<br />
+						<Button onClick={processApplicationApproval}>megerosit</Button>
+					</li>
+				))}
 			</ul>
 
 			<div>{actionData?.status}</div>
@@ -51,26 +50,26 @@ export default function RegistrationPage({ loaderData }: Route.ComponentProps) {
 				<pre>{JSON.stringify(loaderData, null, 2)}</pre>
 			</div>
 		</div>
-	);
+	)
 }
 
 export const action = async ({ request }: Route.ActionArgs) => {
-	const actionData = await request.formData();
-	const intent = actionData.get("intent")?.toString();
-	const eventId = actionData.get("id")?.toString() ?? "";
+	const actionData = await request.formData()
+	const intent = actionData.get("intent")?.toString()
+	const eventId = actionData.get("id")?.toString() ?? ""
 
 	if (intent == "confirm") {
-		console.log("confirmed");
+		console.log("confirmed")
 
 		// set isConfirmed to true
 
 		const result = await db
 			.update(userAtEventTable)
 			.set({ isConfirmed: true })
-			.where(eq(userAtEventTable.id, Number.parseInt(eventId)));
+			.where(eq(userAtEventTable.id, Number.parseInt(eventId)))
 
-		return { status: "ok" };
+		return { status: "ok" }
 	}
 
-	return { status: "error" };
-};
+	return { status: "error" }
+}
