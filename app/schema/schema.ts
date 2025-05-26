09b1667e18bb44c1294439620b441dfed90e5b10
laszloekovacs <laszloekovacs@gmail.com>
2025-05-26 13:04:44 +0200

    fix naming in user at event view

--- app/schema/schema.ts
+++ app/schema/schema.ts
@@ -1,4 +1,4 @@
-import { eq } from 'drizzle-orm'
+import { eq, sql } from "drizzle-orm"
 import {
 	boolean,
 	date,
@@ -7,52 +7,52 @@ import {
 	pgView,
 	text,
 	unique,
-} from 'drizzle-orm/pg-core'
-import { user } from './auth-schema'
+} from "drizzle-orm/pg-core"
+import { user } from "./auth-schema"
 
-export const eventRecord = pgTable('event_record', {
-	id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
-	userId: text('user_id')
+export const eventRecord = pgTable("event_record", {
+	id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+	userId: text("user_id")
 		.notNull()
-		.references(() => user.id, { onDelete: 'set null' }),
-	title: text('name').notNull(),
-	date: date('date').notNull(),
-	slug: text('slug').notNull().unique(),
-	createdAt: date('created_at')
+		.references(() => user.id, { onDelete: "set null" }),
+	title: text("name").notNull(),
+	date: date("date").notNull(),
+	slug: text("slug").notNull().unique(),
+	createdAt: date("created_at")
 		.notNull()
 		.$default(() => new Date().toISOString()),
 })
 
 export const userAtEventTable = pgTable(
-	'user_at_event',
+	"user_at_event",
 	{
-		id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
-		userId: text('user_id')
+		id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+		userId: text("user_id")
 			.notNull()
-			.references(() => user.id, { onDelete: 'set null' }),
-		eventId: integer('event_id')
+			.references(() => user.id, { onDelete: "set null" }),
+		eventId: integer("event_id")
 			.notNull()
-			.references(() => eventRecord.id, { onDelete: 'set null' }),
-		createdAt: date('created_at')
+			.references(() => eventRecord.id, { onDelete: "set null" }),
+		createdAt: date("created_at")
 			.notNull()
 			.$default(() => new Date().toISOString()),
-		isConfirmed: boolean('is_confirmed').notNull().default(false),
+		isConfirmed: boolean("is_confirmed").notNull().default(false),
 	},
-	(table) => [unique().on(table.eventId, table.userId)]
+	(table) => [unique().on(table.eventId, table.userId)],
 )
 
 /*
- * join the users details with the user at event table
+ * join the users details with the user at event table for convinience
  */
-export const userAtEventView = pgView('user_at_event_view').as((qb) =>
+export const userAtEventView = pgView("user_at_event_view").as((qb) =>
 	qb
 		.select({
 			id: userAtEventTable.id,
 			eventId: userAtEventTable.eventId,
-			userId: user.id,
+			userId: sql`${user.id}`.as("user_id"),
+			userName: sql`${user.name}`.as("user_name"),
 			isConfirmed: userAtEventTable.isConfirmed,
-			userName: user.name,
 		})
 		.from(userAtEventTable)
-		.leftJoin(user, eq(user.id, userAtEventTable.userId))
+		.leftJoin(user, eq(user.id, userAtEventTable.userId)),
 )
