dc1b4a91aef95f373d12de9307391e7b38b23512
laszloekovacs <laszloekovacs@gmail.com>
2025-05-30 09:48:35 +0200

    Rename DashboardEventsEventId component to DashboardEventsPage and adjust layout

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -8,7 +8,7 @@ export default function DashboardIndex() {
 			<header className="border-b bg-white/95 backdrop-blur supports-[backdrop-filter]:bg-white/60">
 				<div className="p-4">
 					<div className="flex items-center justify-between">
-						<Link to="/" className="text-2xl font-bold text-primary">
+						<Link to="/dashboard" className="text-2xl font-bold text-primary">
 							Airsoft Naptár - Szervező
 						</Link>
 						<div className="flex items-center space-x-2">
