258f0adf2be812ba930999a18a4f3403649d82b6
laszloekovacs <laszloekovacs@gmail.com>
2025-06-02 17:57:09 +0200

    fix env check, update path to schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'drizzle-kit'
+import { defineConfig } from "drizzle-kit"
+
+const { DATABASE_URL } = process.env
+if (typeof DATABASE_URL != "string") {
+	throw new Error("DATABASE_URL not defined")
+}
 
 export default defineConfig({
-	out: './drizzle',
-	schema: ['./app/schema/schema.ts', 'app/schema/auth-schema.ts'],
-	dialect: 'postgresql',
+	out: "./drizzle",
+	schema: ["./app/schema/index.ts", "app/schema/auth-schema.ts"],
+	dialect: "postgresql",
 	dbCredentials: {
-		url: process.env.DATABASE_URL!
-	}
+		url: DATABASE_URL,
+	},
 })
