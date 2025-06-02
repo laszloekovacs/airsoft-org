8064ef8c69a784aee180f0987f9b97251dd17568
laszloekovacs <laszloekovacs@gmail.com>
2025-06-02 18:27:00 +0200

    refactor: replace PageHeader component with new implementation and update Dashboard layout

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -3,34 +3,30 @@ import Sitemap from "~/components/SiteLinks"
 import { Button } from "~/components/ui/button"
 import type { Route } from "./+types/dashboard"
 import { AuthorizedOnly } from "~/services/auth.server"
-
+import HomePageHeading from "~/components/HomePageHeading"
+import { AuthenticatedSessionMenu } from "~/components/home/SessionMenu"
 
 export async function loader({ request }: Route.LoaderArgs) {
-	await AuthorizedOnly(request, ["organizer"])
+	const { user } = await AuthorizedOnly(request, ["organizer"])
 
-
+	return {
+		name: user.email,
+		image: user.image,
+	}
 }
 
+export default function DashboardIndex({ loaderData }: Route.ComponentProps) {
+	const { name, image } = loaderData
 
-
-export default function DashboardIndex() {
 	return (
 		<div className="min-h-screen bg-background grid grid-rows-[auto_1fr_auto]">
-			<header className="border-b bg-white/95 backdrop-blur supports-[backdrop-filter]:bg-white/60">
-				<div className="p-4">
-					<div className="flex items-center justify-between">
-						<Link to="/dashboard" className="text-lg font-bold text-primary">
-							Airsoft Naptár - Szervező
-						</Link>
-						<div className="flex items-center space-x-2">
-							<Link to="event/create">
-								Új esemény
-							</Link>
-							<Button size="sm" variant="outline">Bejelentkezés</Button>
-						</div>
-					</div>
-				</div>
-			</header>
+			<HomePageHeading
+				title="szervező"
+				sessionComponent={
+					<AuthenticatedSessionMenu email={name} image={image ?? undefined} />
+				}
+				links={[]}
+			/>
 
 			<Outlet />
 			<Sitemap />
