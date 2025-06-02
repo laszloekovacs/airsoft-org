f9b4a7da8b74004e08e6c616569979c620a5dcce
laszloekovacs <laszloekovacs@gmail.com>
2025-06-02 17:43:35 +0200

    add table click handler

--- app/components/dashboard/organizers_event_list.tsx
+++ app/components/dashboard/organizers_event_list.tsx
@@ -1,3 +1,4 @@
+import { Link, useNavigate } from "react-router"
 import {
     Table,
     TableBody,
@@ -5,21 +6,30 @@ import {
     TableCell,
     TableHead,
     TableHeader,
-    TableRow
+    TableRow,
 } from "~/components/ui/table"
-
 
 type PropsType = {
     events: {
-        date: Date,
+        date: Date
         title: string
+        slug: string
     }[]
 }
 
 export const OrganizersEventList = ({ events }: PropsType) => {
-
     if (events.length == 0) {
-        return <div><p>nincsenek általad szervezett események</p></div>
+        return (
+            <div className="grid place-content-center">
+                <p className="text-muted-foreground">nincsenek általad szervezett események</p>
+            </div>
+        )
+    }
+
+    const navigate = useNavigate()
+
+    const handleRowClick = (slug: string) => {
+        navigate(`/dashboard/event/${slug}`)
     }
 
     return (
@@ -27,17 +37,24 @@ export const OrganizersEventList = ({ events }: PropsType) => {
             <TableCaption>Általad szervezett események</TableCaption>
             <TableHeader>
                 <TableRow>
-                    <TableHead>
-                        Dátum
-                    </TableHead>
-                    <TableHead>
-                        Esemény
-                    </TableHead>
+                    <TableHead>Dátum</TableHead>
+                    <TableHead className="text-left">Esemény</TableHead>
                 </TableRow>
             </TableHeader>
 
             <TableBody>
-
+                {events.map((event) => (
+                    <TableRow key={event.title}
+                        className="cursor-pointer"
+                        onClick={() => handleRowClick(event.slug)}>
+                        <TableCell className="text-left">
+                            {event.date.toDateString()}
+                        </TableCell>
+                        <TableCell>
+                            {event.title}
+                        </TableCell>
+                    </TableRow>
+                ))}
             </TableBody>
         </Table>
     )
