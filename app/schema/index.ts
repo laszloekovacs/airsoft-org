a66d46e09af92e345d34e59a6e3d0700222942ad
laszloekovacs <laszloekovacs@gmail.com>
2025-06-06 12:59:08 +0200

    add factions in the schema

--- app/schema/index.ts
+++ app/schema/index.ts
@@ -39,5 +39,29 @@ export const userAtEventTable = pgTable(
 			.$default(() => new Date().toISOString()),
 		group: text("group").notNull().default(""),
 	},
+	/* user allowed only once to apply for a single event */
 	(table) => [unique().on(table.eventId, table.userId)],
 )
+
+/**
+ * Description of a faction in a game
+ */
+export const factionInfoTable = pgTable(
+	"faction_info",
+	{
+		id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+		eventId: integer("eventId").notNull().references(()=> eventRecordTable.id, {onDelete: "cascade"}),
+		title: text().notNull()
+	},
+	/* factions are uniqe inside one game */
+	(table) => [unique().on(table.eventId, table.title)]
+)
+
+/*
+* Describes how users are allocated int factions at a game. allows multiple roles when 
+* multiple games are played: eg: player can be attacker in one game, and a hostage in the next round 
+* in the same event day but different scenario.
+*/
+export const factionAssignmentTable = pgTable("users_in_factions",{
+
+})
