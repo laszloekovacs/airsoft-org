739dc71e824d3aa12d33dfa90c105d1e93e3a614
laszloekovacs <laszloekovacs@gmail.com>
2025-06-10 08:42:13 +0200

    formatted sql script

--- drizzle/0001_search.sql
+++ drizzle/0001_search.sql
@@ -2,26 +2,17 @@
 CREATE OR REPLACE FUNCTION update_search_vector() RETURNS trigger 
 AS $$
 BEGIN 
-NEW.search_vector := setweight(
-    to_tsvector('hungarian', COALESCE(NEW.title, '')),
-    'A'
-) || setweight(
-    to_tsvector('hungarian', COALESCE(NEW.description, '')),
-    'B'
-) || setweight(
-    to_tsvector(
-        'hungarian',
-        array_to_string(COALESCE(NEW.tags, '{}'), ' ')
-    ),
-    'C'
-);
+
+NEW.search_vector := setweight(to_tsvector('hungarian', COALESCE(NEW.title, '')),'A')
+|| setweight(to_tsvector('hungarian', COALESCE(NEW.description, '')),'B') 
+|| setweight(to_tsvector('hungarian',array_to_string(COALESCE(NEW.tags, '{}'), '')),'C');
 
 RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 -- triggers update when event is inserted or updated
-CREATE OR REPLACE TRIGGER tsvectorupdate 
+CREATE OR REPLACE TRIGGER trigger_update_search_vector
 BEFORE INSERT OR UPDATE 
 ON event_record 
 FOR EACH ROW 
