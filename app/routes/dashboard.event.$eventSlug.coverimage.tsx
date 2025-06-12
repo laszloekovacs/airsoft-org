e0561a4297256d26d019de4629fa5dfbc5204d97
laszloekovacs <laszloekovacs@gmail.com>
2025-06-12 19:35:48 +0200

    add file extraction in action

--- app/routes/dashboard.event.$eventSlug.coverimage.tsx
+++ app/routes/dashboard.event.$eventSlug.coverimage.tsx
@@ -1,5 +1,7 @@
 import { Form, useFetcher } from "react-router";
 import { Input } from "~/components/ui/input";
+import type { Route } from "./+types/dashboard.event.$eventSlug.coverimage"
+
 
 
 export default function CoverImageSelectPage() {
@@ -10,10 +12,23 @@ export default function CoverImageSelectPage() {
             <h1>Borítókép</h1>
             <p>válassz borítóképet az eseményhez</p>
 
-            <fetcher.Form method="POST">
+            <fetcher.Form method="POST" encType="multipart/form-data">
                 <Input type="file" accept="image" name="image" />
 
             </fetcher.Form>
         </div>
     )
+}
+
+
+export const action = async ({ request }: Route.ActionArgs) => {
+    const formData = await request.formData();
+    const file = formData.get("image");
+
+    if (!file || typeof file === "string") {
+        throw new Response("No file uploaded", { status: 400 });
+    }
+
+    // file is now a File or Blob object
+    console.log("file uploaded", file.name)
 }
