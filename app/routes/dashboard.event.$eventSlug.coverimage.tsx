8fe77d5a02f61e68b08244558391db9a9cc8d9bf
laszloekovacs <laszloekovacs@gmail.com>
2025-06-16 17:39:47 +0200

    add upload feedback

--- app/routes/dashboard.event.$eventSlug.coverimage.tsx
+++ app/routes/dashboard.event.$eventSlug.coverimage.tsx
@@ -1,11 +1,20 @@
-import { Form, useFetcher } from "react-router";
+import { data, useActionData, useFetcher } from "react-router";
 import { Input } from "~/components/ui/input";
-import type { Route } from "./+types/dashboard.event.$eventSlug.coverimage"
+import type { Route } from "./+types/dashboard.event.$eventSlug.coverimage";
 
+type ActionResult = {
+    status: "success" | "failure",
+    path: string
+}
+
+export const loader = () => {
+    return {}
+}
 
 
 export default function CoverImageSelectPage() {
     const fetcher = useFetcher()
+
 
     return (
         <div>
@@ -16,27 +25,46 @@ export default function CoverImageSelectPage() {
                 <Input type="file" accept="image" name="image" />
                 <Input type="submit" name="submit" value="upload" />
             </fetcher.Form>
+
+            <div>
+                {fetcher.data ? (<p>{fetcher.data.status}</p>) : null}
+            </div>
         </div>
     )
 }
 
 
-export const action = async ({ request }: Route.ActionArgs) => {
+export const action = async ({ request, params }: Route.ActionArgs) => {
     const formData = await request.formData();
     const file = formData.get("image");
 
+    // narrow type to file
     if (!file || typeof file === "string") {
-        throw new Response("No file uploaded", { status: 400 });
+        throw data("No file uploaded", { status: 400 });
+    }
+
+
+    // store it in public/uploads/year/month/user-slug.jpg
+    const now = new Date()
+    const username = "mike"
+
+    const path = `./public/uploads/${now.getFullYear()}/${now.getMonth()}/${username}-${params.eventSlug}.jpg`
+
+
+    const writtenBytes = await Bun.write(path, file, {
+        createPath: true
+    })
+
+    // confirm bytes have been written
+    if (file.size != writtenBytes) {
+        throw data("failed to write file to disc", { status: 400 })
     }
 
     // file is now a File or Blob object
     console.log("file uploaded", file.name)
 
-    // store it in public/uploads/year/month/user-uuid
-    const now = new Date()
-    const username = "mike"
-
-    const basePath = `/public/uploads/${now.getFullYear()}/${now.getMonth()}/${username}-uuid`
-
-    await Bun.write(basePath, file)
+    return {
+        status: "success",
+        path
+    }
 }
