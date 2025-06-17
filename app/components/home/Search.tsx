4d92eca145c9bc8220a2bbea23c2ad4d0b04faa7
laszloekovacs <laszloekovacs@gmail.com>
2025-06-17 12:09:16 +0200

    add search input

--- app/components/home/Search.tsx
+++ app/components/home/Search.tsx
@@ -1,14 +1,19 @@
+import { useDeferredValue, useEffect, useState } from "react"
 import { useFetcher } from "react-router"
+import { Input } from "../ui/input"
 
 
 
-export default function Search() {
+export default function SearchContainer() {
     const fetcher = useFetcher()
+
+
 
     return (
         <div>
-            <fetcher.Form>
-
+            <fetcher.Form method="post">
+                <Input name="query" onChange={e => fetcher.load(`/api/search?q=${e.target.value}`)
+                } />
             </fetcher.Form>
         </div>
     )
