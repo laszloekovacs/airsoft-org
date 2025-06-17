44387a864fc45b183be4bd22fad43f3a58ec6d61
laszloekovacs <laszloekovacs@gmail.com>
2025-06-17 11:46:54 +0200

    add search component

---
+++ app/components/home/Search.tsx
@@ -0,0 +1,26 @@
+import { useFetcher } from "react-router"
+
+
+
+export default function Search() {
+    const fetcher = useFetcher()
+
+    return (
+        <div>
+            <fetcher.Form>
+
+            </fetcher.Form>
+        </div>
+    )
+}
+
+
+
+const SearchField = () => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
