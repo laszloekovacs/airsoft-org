4b092bdc482ec0d6439db1aaab9992eda7f6cc10
laszloekovacs <laszloekovacs@gmail.com>
2025-06-17 10:27:39 +0200

    add fields and desc to metadata page

--- app/routes/dashboard.event.$eventSlug.metadata.tsx
+++ app/routes/dashboard.event.$eventSlug.metadata.tsx
@@ -1,11 +1,29 @@
+import { Label } from "@radix-ui/react-label"
+import { useFetcher } from "react-router"
+import { Input } from "~/components/ui/input"
 
 
 export default function MetadataPage() {
+    const fetcher = useFetcher()
+
 
     return (<div>
         <h1>Metaadatok</h1>
 
+        <fetcher.Form>
+            <Label htmlFor="isPrivate">Privát esemény</Label>
+            <input type="checkbox" name="isPrivate" />
+            <p>Az eseményed nem fog listázásra kerülni, csak linken keresztül lehet megtekinteni</p>
 
+            <Label htmlFor="isOffsite">Linkelt esemény</Label>
+            <input type="checkbox" name="isOffsite" />
+            <p>az eseményt egy másik oldalon szervezed</p>
+
+            <Input type="text" name="offsiteLink" />
+            <p>url a külső esemény oldalhoz</p>
+
+
+        </fetcher.Form>
 
 
     </div>)
