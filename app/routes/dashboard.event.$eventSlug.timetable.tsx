dc73439664bab74e1da8e280d88fe22810206888
laszloekovacs <laszloekovacs@gmail.com>
2025-06-18 20:25:29 +0200

    timetable scaffolded

--- app/routes/dashboard.event.$eventSlug.timetable.tsx
+++ app/routes/dashboard.event.$eventSlug.timetable.tsx
@@ -1,10 +1,51 @@
+import { Form, useActionData, type ActionFunctionArgs } from "react-router";
+import { z } from "zod/v4";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import type { Route } from "./+types/dashboard.event.$eventSlug.timetable"
+
+
+const schema = z.object({
+    label: z.string().min(3).max(128),
+    timestamp: z.string()
+})
 
 
 export default function TimetablePage() {
 
+    const lastResult = useActionData<typeof action>()
+    console.log(lastResult)
+
+    const flattened = lastResult && !lastResult?.success ? z.prettifyError(lastResult.error) : null;
+    console.log(flattened)
+
     return (
         <div>
-            <h1>Dátumok és dőpontok</h1>
+            <h1>Dátumok és időpontok</h1>
+
+            <Form method="post" >
+
+                <label>date</label>
+                <Input type="text" name="timestamp" />
+
+
+                <label>label</label>
+                <Input type="text" name="label" />
+
+
+                <Button type="submit">rogzit</Button>
+            </Form>
+
         </div>
     )
+}
+
+
+export async function action({ request }: Route.ActionArgs) {
+
+    const formData = await request.formData()
+    const submission = schema.safeParse(Object.fromEntries(formData))
+
+
+    return submission
 }
