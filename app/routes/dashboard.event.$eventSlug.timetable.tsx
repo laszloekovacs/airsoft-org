cfd2881ade55e3d2e7bf7ed71617d014ce6007e2
laszloekovacs <laszloekovacs@gmail.com>
2025-06-19 17:52:07 +0200

    add save to db

--- app/routes/dashboard.event.$eventSlug.timetable.tsx
+++ app/routes/dashboard.event.$eventSlug.timetable.tsx
@@ -1,15 +1,18 @@
-import { Form, useActionData, type ActionFunctionArgs } from "react-router";
-import { Button } from "~/components/ui/button";
-import { Input } from "~/components/ui/input";
+import { Form, useActionData, type ActionFunctionArgs } from "react-router"
+import { Button } from "~/components/ui/button"
+import { Input } from "~/components/ui/input"
 import type { Route } from "./+types/dashboard.event.$eventSlug.timetable"
 import { getZodConstraint, parseWithZod } from "@conform-to/zod/v4"
-import { z } from "zod/v4";
+import { z } from "zod/v4"
 import { getFormProps, getInputProps, useForm } from "@conform-to/react"
-
+import { AuthorizedOnly } from "~/services/auth.server"
+import database from "~/services/db.server"
+import { eventRecordTable, timelineTable } from "~/schema"
+import { eq, and } from "drizzle-orm"
 
 const schema = z.object({
+    timestamp: z.iso.datetime({ local: true }),
     label: z.string().min(3).max(128),
-    timestamp: z.iso.datetime()
 })
 
 // TODO: move this to services?
@@ -17,7 +20,10 @@ z.config(z.locales.hu())
 
 export default function TimetablePage() {
     const lastResult = useActionData<typeof action>()
-    const [form, fields] = useForm({ lastResult, constraint: getZodConstraint(schema) })
+    const [form, fields] = useForm({
+        lastResult,
+        constraint: getZodConstraint(schema),
+    })
 
     return (
         <div>
@@ -27,25 +33,60 @@ export default function TimetablePage() {
                 <div>{form.errors}</div>
 
                 <label>date</label>
-                <Input {...getInputProps(fields.timestamp, { type: "datetime-local" })} />
+                <Input
+                    {...getInputProps(fields.timestamp, { type: "datetime-local" })}
+                />
                 <div>{fields.timestamp.errors}</div>
 
                 <label>label</label>
-                <Input {...getInputProps(fields.timestamp, { type: "text" })} />
+                <Input {...getInputProps(fields.label, { type: "text" })} />
                 <div>{fields.label.errors}</div>
 
                 <Button type="submit">rogzit</Button>
             </Form>
-
         </div>
     )
 }
 
-
-export async function action({ request }: Route.ActionArgs) {
-
+export async function action({ request, params }: Route.ActionArgs) {
     const formData = await request.formData()
     const submission = parseWithZod(formData, { schema })
+
+    // check if user has permission, owner of the event
+    const { user } = await AuthorizedOnly(request, ["organizer"])
+
+    // check if submission is correct?
+    if (submission.status == "error") {
+        return submission.reply()
+    }
+
+    // if slug and owner match user can edit event
+    const [event] = await database
+        .select()
+        .from(eventRecordTable)
+        .where(
+            and(
+                eq(eventRecordTable.slug, params.eventSlug),
+                eq(eventRecordTable.ownerId, user.id),
+            ),
+        )
+
+    if (!event)
+        throw new Response(
+            "nincs ilyen esemény, vagy nincs jogosultságod szerkesztéshez",
+            { status: 500 },
+        )
+
+    const { label, timestamp } = submission.payload
+
+    console.log(submission)
+
+    // insert a new time / label into timetable
+    const result = await database.insert(timelineTable).values({
+        eventId: event.id,
+        label,
+        timestamp: new Date(timestamp.toString())
+    })
 
 
     return submission.reply()
