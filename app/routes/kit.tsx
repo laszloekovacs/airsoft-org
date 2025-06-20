0d58512e21986e40911ad208bf577d411a005768
laszloekovacs <laszloekovacs@gmail.com>
2025-06-20 18:33:52 +0200

    add react crop image

--- app/routes/kit.tsx
+++ app/routes/kit.tsx
@@ -1,84 +1,30 @@
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-
-import { useDragAndDrop } from "@formkit/drag-and-drop/react"
-import { NavigationBar } from "~/components/navigation-bar"
-import { Home, User } from "lucide-react"
-import { logger } from '~/services/logging.server'
+import { useState } from "react"
+import Cropper from "react-easy-crop"
 
 
-export const loader = () => {
+const Demo = () => {
+    const [crop, setCrop] = useState({ x: 0, y: 0 })
+    const [zoom, setZoom] = useState(1)
 
-	logger.info("loading Kits endpoint")
 
-	return {}
+    return (
+        <div>
+
+            <div className="w-[480px] h-[300px] relative">
+
+                <Cropper
+                    image="http://picsum.photos/800/600"
+                    crop={crop}
+                    zoom={zoom}
+                    aspect={4 / 3}
+                    onCropChange={setCrop}
+                    onZoomChange={setZoom}
+                    zoomSpeed={0.1}
+                />
+            </div>
+        </div>
+    )
 }
 
 
-
-export default function Kits() {
-	const [parent, tapes] = useDragAndDrop<HTMLUListElement, string>(
-		["Kraftwerk", "Duran Duran", "depeche mode"],
-		{
-			group: "tapes",
-		},
-	)
-
-	return (
-		<div>
-			<div>
-				<NavigationBar links={[
-					{
-						name: "játékosos",
-						icon: <Home />,
-						to: "."
-					},
-					{
-						name: "csapatok",
-						icon: <User />,
-						to: "groups"
-					}
-				]} />
-			</div>
-
-			<ul ref={parent}>
-				{tapes.map((tape) => (
-					<li data-label={tape} key={tape}>
-						{tape}
-					</li>
-				))}
-			</ul>
-
-
-			<div>
-				<Example />
-			</div>
-		</div>
-	)
-}
-
-
-
-function Example() {
-	return (
-		<Menu>
-			<MenuButton>My account</MenuButton>
-			<MenuItems anchor="bottom">
-				<MenuItem>
-					<a className="block data-focus:bg-blue-100" href="/settings">
-						Settings
-					</a>
-				</MenuItem>
-				<MenuItem>
-					<a className="block data-focus:bg-blue-100" href="/support">
-						Support
-					</a>
-				</MenuItem>
-				<MenuItem>
-					<a className="block data-focus:bg-blue-100" href="/license">
-						License
-					</a>
-				</MenuItem>
-			</MenuItems>
-		</Menu>
-	)
-}
+export default Demo
