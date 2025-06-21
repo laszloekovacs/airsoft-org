d475310e70139e682349a55ab177640c54b6ab84
laszloekovacs <laszloekovacs@gmail.com>
2025-06-21 14:30:43 +0200

    add conform to desc

--- app/routes/dashboard.event.$eventSlug.description.tsx
+++ app/routes/dashboard.event.$eventSlug.description.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react"
-import { Form } from "react-router"
+import { Form, useActionData } from "react-router"
 import { Button } from "~/components/ui/button"
 import { event_records } from "~/schema"
 import { AuthorizedOnly } from "~/services/auth.server"
 import database from "~/services/db.server"
 import type { Route } from "./+types/dashboard.event.$eventSlug.description"
 import { and, eq } from "drizzle-orm"
+import { getInputProps, useForm } from "@conform-to/react"
+import { z } from "zod/v4"
+import { parseWithZod } from "@conform-to/zod/v4"
 
 export const loader = async ({ params, request }: Route.LoaderArgs) => {
     const { user } = await AuthorizedOnly(request, ["organizer"])
@@ -26,12 +29,22 @@ export const loader = async ({ params, request }: Route.LoaderArgs) => {
 }
 
 
+const formSchema = z.object({
+    description: z.string()
+})
+
+
 export default function DescriptionEditPage({
     loaderData,
 }: Route.ComponentProps) {
     const { description } = loaderData
 
     const [text, setText] = useState<string>(description || "")
+    const lastResult = useActionData<typeof action>()
+
+    const [form, fields] = useForm({
+        defaultValue: loaderData
+    })
 
     return (
         <div>
@@ -40,11 +53,18 @@ export default function DescriptionEditPage({
 
 
                 <textarea className="border"
-                    value={text}
-                    onChange={(e) => setText(e.target.value)}
+                    {...getInputProps(fields.description, { type: "text" })}
                 ></textarea>
 
             </Form>
         </div>
     )
+}
+
+
+export const action = async ({ request }: Route.ActionArgs) => {
+    const formData = await request.formData()
+    const submission = parseWithZod(formData, { schema: formSchema })
+
+
 }
