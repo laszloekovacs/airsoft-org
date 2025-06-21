1d521841d92d8242082e5f9b0e71e4c25d5cc358
laszloekovacs <laszloekovacs@gmail.com>
2025-06-21 15:44:46 +0200

    add search component

---
+++ app/components/search/search.tsx
@@ -0,0 +1,22 @@
+import { Form } from "react-router"
+
+const SearchField = () => {
+    return (<div>
+        <Form method="post">
+
+        </Form>
+    </div>)
+}
+
+
+export const SearchContainer = () => {
+
+    return (
+        <div>
+            <SearchField />
+        </div>
+    )
+}
+
+
+
