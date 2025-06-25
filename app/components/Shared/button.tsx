6cc6d2e2f57a39f1f0846f2c58cd15313a044cfa
laszloekovacs <laszloekovacs@gmail.com>
2025-06-25 17:26:53 +0200

    add custom button

--- app/components/Shared/button.tsx
+++ app/components/Shared/button.tsx
@@ -5,30 +5,27 @@ const styles = cva({
     variants: {
         intent: {
             primary: "border-transparent bg-blue-500 text-white hover:bg-blue-600",
-            secondary: "border-gray-400 bg-white text-gray-800 hover:bg-gray-100"
+            secondary: "border-gray-400 bg-white text-gray-800 hover:bg-gray-100",
         },
         size: {
             small: "px-2 py-1 text-sm",
-            medium: "px-4 py-4 text-base"
-        }
+            medium: "px-4 py-4 text-base",
+        },
     },
     defaultVariants: {
         intent: "primary",
-        size: "medium"
-    }
+        size: "medium",
+    },
 })
 
-export interface ButtonProps
-    extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof styles> { }
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+    VariantProps<typeof styles>
 
 export const Button: React.FC<ButtonProps> = ({
     className,
     intent,
     size,
     ...props
-}) => (
-    <button
-        className={styles({ intent, size, className })}
-        {...props}
-    />
-)
+}) => {
+    return <button className={styles({ intent, size, className })} {...props} />
+}
